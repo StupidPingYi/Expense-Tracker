056ac669503ee54dce24af8b3d8d8d5f8204a582
Z <jiazhuo@jiazhuos-mbp.wireless.duke.local>
2014-11-10 14:37:15 -0500

    Only a test

---
+++ first.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Expense Tracker
+//
+//  Created by 贾卓 on 11/9/14.
+//  Copyright (c) 2014 Duke University. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
